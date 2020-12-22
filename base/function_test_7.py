911d49b4215d423e18c74c87abcb712289f14cbc
panjiufeng <185239322pan>
2020-12-22 23:05:19 +0800

    闭包和面向对象

--- base/function_test_7.py
+++ base/function_test_7.py
@@ -3,14 +3,38 @@
 # class 首字母小写， 类名首字母大写
 
 class Student:
-    def __init__(self, name, score):
+    company = 'Python'
+    count = 88
+
+    def __init__(self, temp_name, temp_age):
         super().__init__()
-        self.name = name
-        self.score = score
+        self.name = temp_name
+        self.age = temp_age
+        Student.count = Student.count + 1
 
-    def say_score(self):
-        print(self.name, '的分数是:', self.score)
+    def change_age(self, new_age):
+        self.age = new_age
+
+    def get_name(self):
+        return self.name
+
+    @classmethod
+    def print_information(cls):
+        print(cls.company)
+        print(cls.count)
+
+    def __str__(self):
+        return self.name, self.age
 
 
-student = Student('王二狗', 93)
-student.say_score()
+if __name__ == '__main__':
+    student = Student('王二狗', 93)
+    student.change_age(210)
+    print(student.get_name())
+    print(student.age)
+
+    print(Student.company, Student.count)
+    Student.print_information()
+
+    print(Student)
+
